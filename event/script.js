22740cbef3fb69d792a427ba2d0132f290b70257
Rcthapa1996 <Rcthapa1996@gmail.com>
2023-04-12 01:41:43 +0545

    learn event propogation

---
+++ event/script.js
@@ -0,0 +1,12 @@
+function outerMost() {
+  console.log("I am outer Most!!");
+}
+function outer() {
+  console.log("I am outer!!");
+}
+function inner() {
+  console.log("I am Inner !!");
+}
+function innerMost() {
+  console.log("I am inner Most!!");
+}
